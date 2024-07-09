4295cb7666edccb8b2f74093988da7ecc3542b98
Max <sgenn221905@gmail.com>
2024-07-09 14:11:43 +0500

    Created insight, partners and testimonials sections; Made homework for the partners section (auto-slider)

---
+++ src/js/components/home/slider-partners.js
@@ -0,0 +1,67 @@
+import Swiper from 'swiper';
+import { Autoplay } from 'swiper/modules';
+
+export const usePartnersSlider = () => {
+  let partnersSlider = null;
+
+  function checkWidth() {
+    if (window.innerWidth <= 768) {
+      if (!partnersSlider) {
+        initSwiper();
+      }
+    } else {
+      if (partnersSlider) {
+        destroySwiper();
+      }
+    }
+  }
+
+  function initSwiper() {
+    partnersSlider = new Swiper('.partners__slider', {
+      modules: [Autoplay],
+      speed: 700,
+      loop: true,
+      slidesPerView: 1.5,
+      spaceBetween: 15,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+        651: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+        451: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+      },
+    });
+  }
+
+  function destroySwiper() {
+    if (partnersSlider) {
+      partnersSlider.destroy(true, true);
+      partnersSlider = null;
+    }
+  }
+
+  window.addEventListener('resize', checkWidth);
+  checkWidth();
+};
+
+export const useInsightSlider = () => {
+  new Swiper('.insight__slider', {
+    slidesPerView: 'auto',
+    spaceBetween: 32,
+    loop: true,
+    centeredSlides: true,
+    breakpoints: {
+      1201: {
+        centeredSlides: false,
+      },
+    },
+  });
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,12 @@
+import 'swiper/css'
 import '../assets/scss/main.scss';
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
+import { usePartnersSlider } from './components/home/slider-partners.js'
+import { useInsightSlider, useTestimonialsSlider } from './components/home/slider.js';
 
 useTheme();
 useBurger();
+useInsightSlider();
+usePartnersSlider ();
+useTestimonialsSlider ();
