beab498d16605a0a9349dbe9f5a6f438caedfffa
Max <sgenn221905@gmail.com>
2024-07-10 13:22:39 +0500

    Added pricing page and hero section in it; done homework in Pricing lesson section

---
+++ src/js/components/pricing/priceSwitcher.js
@@ -0,0 +1,46 @@
+export const usePriceSwitcher = () => {
+  const switcher = document.querySelector('[data-price="switcher"]');
+  const starterPrice = document.querySelector('[data-price="starter"');
+  const popularPrice = document.querySelector('[data-price="popular"');
+  const enterprisePrice = document.querySelector('[data-price="enterprise"');
+
+  const priceList = {
+    starter: {
+      default: 120,
+      withSale: 100,
+    },
+    popular: {
+      default: 1680,
+      withSale: 1400,
+    },
+    enterprise: {
+      default: 2520,
+      withSale: 2100,
+    },
+  };
+
+  const setDefaultPrices = () => {
+      starterPrice.textContent = priceList.starter.default;
+      popularPrice.textContent = priceList.popular.default;
+      enterprisePrice.textContent = priceList.enterprise.default;
+  }
+
+  const setPricesWithSale = () => {
+    starterPrice.textContent = priceList.starter.withSale;
+    popularPrice.textContent = priceList.popular.withSale;
+    enterprisePrice.textContent = priceList.enterprise.withSale;
+  };
+
+  switcher.checked = true;
+  setPricesWithSale();
+
+  switcher.addEventListener('click', () => {
+    if (switcher.checked) {
+      setPricesWithSale();
+    } else {
+      setDefaultPrices()
+    }
+  });
+
+  console.log
+};
---
+++ src/js/pricing.js
@@ -0,0 +1,11 @@
+import '../assets/scss/pricing.scss';
+
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+import { usePriceSwitcher } from './components/pricing/priceSwitcher.js';
+import { accordeonFooter } from './components/accordeon-footer.js';
+
+useTheme();
+useBurger();
+usePriceSwitcher ();
+accordeonFooter ();
