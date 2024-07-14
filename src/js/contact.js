e0593ec64cd88234d76e96ff0a623586363e62ab
Max <sgenn221905@gmail.com>
2024-07-14 10:51:03 +0500

    Created contact:get-started section

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,13 +1,16 @@
+import 'swiper/css';
 import '../assets/scss/contact.scss';
 
 import { useMap } from './components/contact/map.js';
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
+import { usePhone } from './components/contact/phone.js';
+import { usePartnersSlider } from './components/home/slider-partners.js';
 import { accordeonFooter } from './components/accordeon-footer.js';
 
-useMap ();
+useMap();
 useTheme();
+usePartnersSlider();
 useBurger();
+usePhone();
 accordeonFooter();
-
-
